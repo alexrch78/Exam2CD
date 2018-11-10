6186e9c7e0a168668aa0d1aefbb705544f1a6280
DESKTOP-AVQ3BDH\Alex <alexander@playbuzz.com>
2018-11-10 20:56:30 +0200

    WIP

---
+++ Exam2CD/Tests/ClickDimensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2CD.Tests
+{
+    [TestFixture]
+    public class ClickDimensionsTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Initiate()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void ClickDimensions_CentralAmerica()
+        {
+
+        }
+
+        [Test]
+        public void ClickDimensions_Academy()
+        {
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
